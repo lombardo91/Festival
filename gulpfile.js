745e386a2857d7ba2d3454e6a89dde0f53d4dd57
Mauro <lenardon_1@hotmail.com>
2023-09-28 18:47:01 -0300

    Primer Commit desde Logui

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+const {src, dest, watch} = require("gulp")
+//requiere: extraer toda la funcionalidad de gulp 
+//gulp: dependencia en el package.json 
+const sass = require("gulp-sass")(require('sass'));
+const plumber = require('gulp-plumber');
+
+
+
+/*
+function tarea(done){
+    console.log('Hola Mundo');
+
+    done();  //colkack - parafinalizar tarea / quita error en consola al ejecutar npx gulp NombreDeLaFuncion
+}
+
+exports.acaPongo = tarea;
+
+
+//Si llamo la funcion desde aca es npx gulp NombreFuncion
+//Si llamo desde package.json es npm run "nombreDelSrcipt"  
+// ---> del otro lado del script : gulp NombreFuncion
+*/
+function css(done) {
+    //identificar el archivo de SASS
+    src('src/scss/**/*.scss')
+        .pipe( plumber()) // evita que se detenga la ejecucion por errores
+        //Compilarlo   //El Sass que traigo del package.json
+        .pipe(sass() )
+        //Almacenar el archivo
+        .pipe(dest("build/css"))
+
+
+    done(); //Callback avisa a Gulp cuando llegamos al final
+}
+
+function dev(done) {
+    watch('src/scss/**/*.scss', css)
+    // watch, deja siempre escuchando el archivo para detectar si sufre cambios
+
+    done();
+}
+
+
+exports.css = css;
+exports.dev = dev;
+
+//DEsde aca llamamos a las funciones con : npx gulp nombreDeLaFuncion
+// Si lo llamo desde el package.json utilizo : npm run nombreDeLaFuncion
