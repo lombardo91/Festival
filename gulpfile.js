8bd0298ba2d6d0e131ea581c582fca6d4afe420b
Mauro <lenardon_1@hotmail.com>
2023-09-29 17:43:27 -0300

    Comenzando la galeria de imagenes con JS

--- gulpfile.js
+++ gulpfile.js
@@ -77,19 +77,28 @@ function versionAvif(done) {
     done();
 }
 
+function javascript(done){
+    src('src/js/**/*.js')
+    .pipe(dest('build/js'));
+
+    done();
+}
+
 function dev(done) {
     watch('src/scss/**/*.scss', css)
     // watch, deja siempre escuchando el archivo para detectar si sufre cambios
+    watch('src/js/**/*.js', javascript)
 
     done();
 }
 
 
 exports.css = css;
+exports.js = javascript;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
 exports.versionAvif = versionAvif;
-exports.dev = parallel(imagenes, versionWebp, versionAvif, dev);  // parallel: ejecuta funciones en simultaneo
+exports.dev = parallel(imagenes, versionWebp, versionAvif, javascript, dev);  // parallel: ejecuta funciones en simultaneo
 
 
 
