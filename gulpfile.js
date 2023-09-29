702e48268ebc34fbf0705878804cfc4e9597469a
Mauro <lenardon_1@hotmail.com>
2023-09-29 11:50:50 -0300

    Termiando modificacion de extencion en imagenes

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const {src, dest, watch} = require('gulp');
+const {src, dest, watch, parallel} = require('gulp');
 //requiere: extraer toda la funcionalidad de gulp 
 //gulp: dependencia en el package.json 
 
@@ -7,9 +7,11 @@ const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
 
 // Imagenes -----
+const cache = requiere('gulp-cache'); // npm i --save-dev gulp-cache
+const imagemin = requiere('gulp-imagemin'); // npm i --save-dev gulp-imagemin@7.1.0
 const webp = require('gulp-webp');
 
-
+clearImmediate
 /*
 function tarea(done){
     console.log('Hola Mundo');
@@ -37,15 +39,28 @@ function css(done) {
     done(); //Callback avisa a Gulp cuando llegamos al final
 }
 
-function versionWebp(done) {
-
+function imagenes(done) {
     const opciones = {
-        quality: 50
+        optimizationLevel: 3
+    }
+
+    src('src/scss/img/**/*.{png,jpg}')
+    .pipe( cache ( imagenes(opciones)))
+    .pipe(dest("build/img"))
+
+    done();
+}
+
+function versionWebp(done) {
+    // Webp: toma opciones, vajamos la calidad de 100 a 50
+    const opciones = {
+        quality: 50   
     };
 
-    src('src/img/**/*.{jpg, png}')
+    src('src/scss/img/**/*.{png,jpg}') // {.....} ==> Busca, los formatos que le indique dentro de esas {}
         .pipe( webp(opciones))
-        .pipe( dest('build/img'))
+        .pipe( dest("build/img"))
+
     done();
 }
 
@@ -58,8 +73,11 @@ function dev(done) {
 
 
 exports.css = css;
-exports.dev = dev;
+exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
+exports.dev = parallel(imagenes, versionWebp, dev);  // parallel: ejecuta funciones en simultaneo
+
+
 
 //DEsde aca llamamos a las funciones con : npx gulp nombreDeLaFuncion
 // Si lo llamo desde el package.json utilizo : npm run nombreDeLaFuncion
